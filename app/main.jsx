c25d8397931c747be71f373dbebadc86a3bbf201
eScribMac <evanscribner1@gmail.com>
2017-01-10 17:46:09 -0500

    auth

--- app/main.jsx
+++ app/main.jsx
@@ -7,8 +7,8 @@ import {connect, Provider} from 'react-redux'
 import store from './store'
 import HomePage from './components/HomePage'
 import Login from './components/Login'
+import Signup from './components/Signup'
 import WhoAmI from './components/WhoAmI'
-import NavBar from './components/NavBar'
 import product from './components/Product'
 import {fetchProductsFromServer} from './reducers/actionCreators'
 
@@ -21,14 +21,14 @@ render (
     <Router history={browserHistory}>
     	<Route path='/'>
     	<Route path='/products/:id' component={product}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/signup' component={Signup}/>
     		{/**<Route path='/products' component={products}/>
     		    		<Route path='/products/category?' component={products}/>
 
     		    		<Route path='/products/filter?' component={products}/>
     		    		<Route path='/cart' component={cart}/>
     		    		<Route path='/products' component={products}/>
-    		    		<Route path='/login' component={login}/>
-    		    		<Route path='/signup' component={signup}/>
     		    		<Route path='/myAccount' component={Account}/>
     		    		<Route path='/myAccount/orderHistory' component={orderHistory}/>
     		    		<Route path='/myAccount/orderHistory/:id' component={order}/>
