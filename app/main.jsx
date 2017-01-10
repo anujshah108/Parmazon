ecc729f1dcd76982af0ca4ef6afcd902430ca5b4
pchmura <pchmura8@gmail.com>
2017-01-10 15:36:06 -0500

    added react router routes

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, IndexRedirect, browserHistory, IndexRoute} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
@@ -13,8 +13,23 @@ import NavBar from './components/NavBar'
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={HomePage}>
-      </Route>
+    	<Route path='/'>
+    		{/**<Route path='/products' component={products}/>
+    		    		<Route path='/products/category?' component={products}/>
+    		    		<Route path='/products/:id' component={product}/>
+    		    		<Route path='/products/filter?' component={products}/>
+    		    		<Route path='/cart' component={cart}/>
+    		    		<Route path='/products' component={products}/>
+    		    		<Route path='/login' component={login}/>
+    		    		<Route path='/signup' component={signup}/>
+    		    		<Route path='/myAccount' component={Account}/>
+    		    		<Route path='/myAccount/orderHistory' component={orderHistory}/>
+    		    		<Route path='/myAccount/orderHistory/:id' component={order}/>
+    		    		<Route path='/checkout' component={checkout}/>
+    		    		<Route path='/checkout' component={checkout}/>
+    		**/}
+    		<IndexRoute component={HomePage}/>
+    	</Route>
     </Router>
   </Provider>,
   document.getElementById('main')
