1522249ec6ca2c67619320fe06b54fc038f7254e
Jason Lam <jaylam111@gmail.com>
2017-01-11 11:53:07 -0500

    favicon

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -7,7 +7,7 @@ export const Signup = ({ signup }) => (
   } }>
     <input name="username" />
     <input name="password" type="password" />
-    <input type="submit" value="Signup" />
+    <input type="submit" value="Signup" align='center'/>
   </form>
 )
 
