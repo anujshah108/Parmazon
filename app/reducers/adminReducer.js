870f75dff5ed99e49770ecbf809bc2f99dda2167
Anuj Shah <ashah812@gmail.com>
2017-01-11 17:46:51 -0500

    deleted admin reducer

--- app/reducers/adminReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import {RECEIVE_ALL_USERS, RECEIVE_USER} from './actionCreators'
-
-
-
-
-
--- server/users.js
+++ server/users.js
@@ -8,35 +8,34 @@ const {mustBeLoggedIn, forbidden,} = require('./auth.filters')
 module.exports = require('express').Router()
 	//requesting client must be logged in as an admin
 	//sends all of the user instances
-	.get('/', forbidden('only admins can list users'), (req, res, next) => 
+	.get('/', forbidden('only admins can list users'), (req, res, next) =>
 		User.findAll()
 		.then(users => res.json(users))
 		.catch(next))
 	//creates a new user instance on the User table
-	.post('/', mustBeLoggedIn, (req, res, next) =>
+	.post('/', (req, res, next) =>
 		User.create(req.body)
 		.then(user => res.status(201).json(user))
 		.catch(next))
 	//gets a specific user
-	.get('/:id', mustBeLoggedIn, (req, res, next) => 
+	.get('/:id', mustBeLoggedIn, (req, res, next) =>
 		User.findById(req.params.id)
 		.then(user => res.json(user))
 		.catch(next))
 	//gets all the orders for a user
-	.get('/:id/orders', mustBeLoggedIn, (req, res, next) => 
+	.get('/:id/orders', mustBeLoggedIn, (req, res, next) =>
 		Order.findAll({where: {user_id: req.params.id}})
 		.then(listOforders => res.json(listOforders))
 		.catch(next))
 	//update a user that is passed through req.params
-	.put('/:id', mustBeLoggedIn, (req, res, next) => 
+	.put('/:id', mustBeLoggedIn, (req, res, next) =>
 		User.findById(req.params.id)
 		.then(user => user.update(req.body)
 		.then(updatedUser => res.json(updatedUser))
 		.catch(next))
 	//deletes a user that is selected from req.params
-	.delete('/:id', mustBeLoggedIn, (req, res, next) => 
+	.delete('/:id', mustBeLoggedIn, (req, res, next) =>
 		User.findById(req.params.id)
 		.then(userToDestroy => userToDestroy.destroy())
 		.then(res.sendStatus(204))
 		.catch(next)))
-	
