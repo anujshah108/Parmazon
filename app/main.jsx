28a2f3551f1cf3e9782b59ed4a08adfda1e95e8e
Anuj Shah <ashah812@gmail.com>
2017-01-17 12:37:45 -0500

    cart persistance fixed small bug

--- app/main.jsx
+++ app/main.jsx
@@ -59,6 +59,7 @@ const onSingleProductEnter = function(nextRouterState) {
   store.dispatch(fetchSingleProduct(nextRouterState.params.productId));
   store.dispatch(fetchRatingforProduct(nextRouterState.params.productId));
   store.dispatch(fetchReviewsforProduct(nextRouterState.params.productId));
+  store.dispatch(fetchOpenCart());
 
 };
 
